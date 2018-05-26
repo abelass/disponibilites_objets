bc2a6358d54ede5cc63bfcc1ddc4f37e3165c43d
Rainer Müller <rainer@websimple.be>
2018-05-26 10:53:56 +0200

    la fabrique

---
+++ lang/disponibilite_date_fr.php
@@ -0,0 +1,47 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_disponibilite_date' => 'Ajouter cette date disponible',
+
+	// C
+	'champ_date_debut_label' => 'Date début',
+	'champ_date_fin_label' => 'Date fin',
+	'champ_disponible_label' => 'Disponible',
+	'champ_titre_label' => 'Titre',
+	'confirmer_supprimer_disponibilite_date' => 'Confirmez-vous la suppression de cette date disponible ?',
+
+	// I
+	'icone_creer_disponibilite_date' => 'Créer une date disponible',
+	'icone_modifier_disponibilite_date' => 'Modifier cette date disponible',
+	'info_1_disponibilite_date' => 'Une date disponible',
+	'info_aucun_disponibilite_date' => 'Aucune date disponible',
+	'info_disponibilite_dates_auteur' => 'Les dates disponibles de cet auteur',
+	'info_nb_disponibilite_dates' => '@nb@ dates disponibles',
+
+	// R
+	'retirer_lien_disponibilite_date' => 'Retirer cette date disponible',
+	'retirer_tous_liens_disponibilite_dates' => 'Retirer toutes les dates disponibles',
+
+	// S
+	'supprimer_disponibilite_date' => 'Supprimer cette date disponible',
+
+	// T
+	'texte_ajouter_disponibilite_date' => 'Ajouter une date disponible',
+	'texte_changer_statut_disponibilite_date' => 'Cette date disponible est :',
+	'texte_creer_associer_disponibilite_date' => 'Créer et associer une date disponible',
+	'texte_definir_comme_traduction_disponibilite_date' => 'Cette date disponible est une traduction de la date disponible numéro :',
+	'titre_disponibilite_date' => 'Date disponible',
+	'titre_disponibilite_dates' => 'Dates disponibles',
+	'titre_disponibilite_dates_rubrique' => 'Dates disponibles de la rubrique',
+	'titre_langue_disponibilite_date' => 'Langue de cette date disponible',
+	'titre_logo_disponibilite_date' => 'Logo de cette date disponible',
+	'titre_objets_lies_disponibilite_date' => 'Liés à cette date disponible',
+);
