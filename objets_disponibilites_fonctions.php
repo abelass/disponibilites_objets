ac87350ff3c612d24b72bf07986135c1455ae1a8
abelass <rainer@websimple.be>
2018-05-28 11:21:51 +0200

    Les crites dans dans les plugin dédié dates_outils

--- objets_disponibilites_fonctions.php
+++ objets_disponibilites_fonctions.php
@@ -13,47 +13,10 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
-include_spip('criteres/objets_disponibilites');
+// Les fonctions de dates_outils.
+include_spip('filtres/dates_outils');
 
-/**
- * Donne les dates d'un intervalle. Par défaut exclus la date fin
- *
- * @param mixed $date_debut
- * @param mixed $date_fin
- * @param integer $diff
- *   nombre
- * @return string[]
- */
-function dates_intervalle($date_debut, $date_fin, $debut = 0, $fin =0, $horaire= false) {
-	$format = 'Y-m-d';
+// Les critères de dates_outils.
+include_spip('criteres/inc_agenda_filtres');
+include_spip('criteres/public_agenda');
 
-	if($horaire) {
-		$format = 'Y-m-d H:i:s';
-	}
-
-	if (!is_integer($date_debut)) {
-		$date_debut = strtotime($date_debut);
-	}
-	if (!is_integer($date_fin)) {
-		$date_fin = strtotime($date_fin);
-	}
-
-	$dates = array();
-	if ($date_fin >= $date_debut) {
-		$difference = $date_fin - $date_debut;
-		$nombre_jours = round($difference / (60 * 60 *24)) + $fin;
-		$i = $debut;
-
-		while ($i <= $nombre_jours) {
-			$muliplie = $i * 60 * 60 *24;
-			$dates[] = date($format, $date_debut + $muliplie);
-			$i++;
-		}
-	}
-
-	return $dates;
-}
-
-function date_relative_brut($date, $decalage, $format = 'Y-m-d') {
-	return date($format, strtotime($decalage, strtotime($date)) );
-}
