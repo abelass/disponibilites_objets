4e39253d4da1504e2bff6315d9345c96fa5ad6e5
abelass <rainer@websimple.be>
2018-05-29 10:44:05 +0200

    mixe fonction squelettes

--- objets_disponibilites_fonctions.php
+++ objets_disponibilites_fonctions.php
@@ -21,3 +21,19 @@ include_spip('filtres/inc_agenda_filtres');
 // Les critères de dates_outils.
 include_spip('criteres/inc_agenda_filtres');
 include_spip('criteres/public_agenda');
+
+function dates_disponibles($options, $contexte) {
+
+	$contexte = array_merge(unserialize($contexte), $options);
+
+	$contexte['disponible'] = '';
+	$dates_indisponibles = unserialize(recuperer_fond('inclure/dates_disponibles_tableau', $contexte));
+
+	$contexte['disponible'] = 1;
+
+	$dates_disponibles = unserialize(recuperer_fond('inclure/dates_disponibles_tableau', $contexte));
+
+	$dates = array_diff($dates_disponibles, $dates_indisponibles);
+
+	return $dates;
+}
