01260d2594bac3b65cd04dfd72b2c7d8a53a5cf5
abelass <rainer@websimple.be>
2018-05-31 09:43:39 +0200

    doc et ménage avec les variables

--- objets_disponibilites_fonctions.php
+++ objets_disponibilites_fonctions.php
@@ -8,7 +8,6 @@
  * @licence    GNU/GPL v3
  * @package    SPIP\Objets_disponibilites\Fonctions
  */
-
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -17,27 +16,45 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 include_spip('filtres/dates_outils');
 include_spip('filtres/inc_agenda_filtres');
 
-
 // Les critères de dates_outils.
 include_spip('criteres/inc_agenda_filtres');
 include_spip('criteres/public_agenda');
 
+/**
+ * Calcule les dates disponibles d'un objet donnée.
+ *
+ * @param array $options
+ *        	Des options passées surchargeant le contexte.
+ * @param mixed $contexte
+ *          Les variables du contexte
+ * @return array
+ *          Les dates disponibles.
+ */
 function dates_disponibles($options, $contexte) {
-
 	$contexte = array_merge(unserialize($contexte), $options);
 
-	spip_log($contexte, 'teste');
+	/*
+	 * Les indisponibles
+	 */
 
-	$dates_utilisees = unserialize(recuperer_fond('inclure/dates_utilisees_tableau', $contexte));
+	// Les dates considérés comme utilisées
+	$dates_utilisees = array();
+	if (isset($contexte['utilisation_squelette']) and $chemin = $contexte['utilisation_squelette'] and find_in_path($chemin . '.html')) {
+		$dates_utilisees = unserialize(recuperer_fond($chemin, $contexte));
+	}
 
-	$dates_indisponibles = array_merge(
-		$dates_utilisees,
-		unserialize(recuperer_fond('inclure/dates_indisponibles_tableau', $contexte))
-	);
-	spip_log($dates_utilisees, 'teste');
-	$dates_disponibles = unserialize(recuperer_fond('inclure/dates_disponibles_tableau', $contexte));
+	// Les dates de l'objet encodés comme indisponibles
+	$dates_indisponibles = unserialize(recuperer_fond('disponibilites/indisponibles', $contexte));
 
-	$dates = array_diff($dates_disponibles, $dates_indisponibles);
+	/*
+	 * Les disponibles
+	 */
+	$dates_disponibles = unserialize(recuperer_fond('disponibilites/disponibles', $contexte));
+
+	/*
+	 * Le décompte, les disponibles moins les indisponibles
+	 */
+	$dates = array_diff($dates_disponibles, $dates_indisponibles, $dates_utilisees);
 
 	return $dates;
 }
